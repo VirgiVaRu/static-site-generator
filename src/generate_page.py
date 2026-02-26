66a78b9f58d986dd3e4d7676f06870ad7dfbc7d6
VirgiVaRu <virginiavruano@gmail.com>
2026-02-26 15:13:16 +0100

    made changes to work with Github Pages

--- src/generate_page.py
+++ src/generate_page.py
@@ -2,7 +2,7 @@ from markdown_to_html_node import markdown_to_html_node
 from extract_title import extract_title
 import os
 
-def generate_page(from_path, template_path, dest_path):
+def generate_page(from_path, template_path, dest_path, basepath):
     print(f"Generating path from {from_path} to {dest_path} using {template_path}")
 
     markdown_file = open(from_path)
@@ -18,17 +18,19 @@ def generate_page(from_path, template_path, dest_path):
 
     final_page = template_contents.replace("{{ Title }}", title)
     final_page = final_page.replace("{{ Content }}", html_content)
+    final_page = final_page.replace("href=\"/", f"href=\"{basepath}")
+    final_page = final_page.replace("src=\"/", f"src=\"{basepath}")
 
     with open(dest_path, "w") as dest:
         dest.write(final_page)
 
-def generate_pages_recursive(dir_path_content, template_path, dest_dir_path):
+def generate_pages_recursive(dir_path_content, template_path, dest_dir_path, basepath):
     markdown_files = os.listdir(dir_path_content)
     for file in markdown_files:
         src_path = os.path.join(dir_path_content, file)
         dest_path = os.path.join(dest_dir_path, file.replace(".md", ".html"))
         if os.path.isfile(src_path):
-            generate_page(src_path, template_path, dest_path)
+            generate_page(src_path, template_path, dest_path, basepath)
         else:
             os.makedirs(dest_path)
-            generate_pages_recursive(src_path, template_path, dest_path)
+            generate_pages_recursive(src_path, template_path, dest_path, basepath)
--- src/main.py
+++ src/main.py
@@ -1,17 +1,22 @@
 import shutil
 import os
+import sys
 from copy_contents import copy_contents
 from generate_page import generate_pages_recursive
 
 def main():
+    if sys.argv:
+        basepath = sys.argv[1]
+    else:
+        basepath = "/"
     static = os.path.abspath("static/")
-    public = os.path.abspath("public/")
+    docs = os.path.abspath("docs/")
     template = os.path.abspath("template.html")
     content = os.path.abspath("content")
-    if os.path.exists(public):
-        shutil.rmtree(public)
-    copy_contents(static, public)
-    generate_pages_recursive(content, template, public)
+    if os.path.exists(docs):
+        shutil.rmtree(docs)
+    copy_contents(static, docs)
+    generate_pages_recursive(content, template, docs, basepath)
     
 
 main()
